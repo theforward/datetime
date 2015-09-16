9c978830a2df2f3ebab777239ad3403245b07c2b
Jon Mifsud <info@jonmifsud.com>
2015-09-16 08:36:51 +0200

    set time to 5am

--- content/content.get.php
+++ content/content.get.php
@@ -15,6 +15,16 @@
 
 		public function view(){
 			$this->_Result = Calendar::formatDate(General::sanitize($_GET['date']), General::sanitize($_GET['time']), NULL, true);
+
+			if ($this->_Result  && !is_numeric($_GET['date']) && substr($_GET['date'], -8) !== "12:00 am"){
+				$tmpResult = json_decode($this->_Result);
+				if ($tmpResult->status == 'valid' && substr($tmpResult->date, -8) == "12:00 am"){
+					$tmpResult->date = substr($tmpResult->date, 0 , -8) . '05:00 am';
+					$tmpResult->timestamp = $tmpResult->timestamp + 5 * 3600;
+					$this->_Result = json_encode($tmpResult);
+					// var_dump(substr($tmpResult->date, 0 , -8));die;
+				}
+			}
 		}
 
 		public function generate($page = null){
